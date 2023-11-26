bbe5bedf3795e05ab07634f1fbb278fcd6ea0609
aditd5 <aditdhiantono@gmail.com>
2023-11-26 23:38:08 +0700

    fix: appbar color

--- lib/main_screen.dart
+++ lib/main_screen.dart
@@ -9,6 +9,7 @@ class MainScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: const Text('Wisata Bandung'),
       ),
       body: ListView.builder(
